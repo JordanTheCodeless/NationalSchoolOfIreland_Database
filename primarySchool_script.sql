77b52969ac643e7b3f73c6f27b93bb5565f2735b
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2024-03-13 15:09:12 +0000

    Created StudentTable and StudentData Script to deploy data to table

--- primarySchool_script.sql
+++ primarySchool_script.sql
@@ -1,3 +1,12 @@
 # Database created 12/03/2024
 CREATE DATABASE primarySchool;
+#Use primarySchool dataBase
+USE primarySchool;
+CREATE TABLE Student (
+    StudentID INT(6) NOT NULL PRIMARY KEY,
+    StudentFName VARCHAR(20) NOT NULL,
+    StudentLName VARCHAR(20) NOT NULL,
+    dob DATE NOT NULL,
+    gender CHAR(1) CHECK (gender IN ('M', 'F'))
+);
 
